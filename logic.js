9e24de7a1d82cc520ebe3573f739ceba189b96de
Deepika <palakondadeepika25@gmail.com>
2025-07-17 09:52:25 +0530

    deploy

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
+alert("📌 Instructions & Uses:\n\n1️⃣ Choose the desired size of your QR code.\n2️⃣ Enter any text or link (e.g., URL's like LinkedIn, Instagram, GitHub project).\n3️⃣ Click 'Generate' to create your QR code.\n\n📱 Scan the QR with your phone to open the link.\n🖼️ You can save the QR as an image in your gallery.\n✨ Share it with others instead of typing your profile names or links manually!🙌");
+
 function genQR() {
-      var gapi = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=HelloWorld";
+    //var gapi = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=HelloWorld";
       const myimg = document.getElementById("img");
 const mytext = document.getElementById("qrtext").value.trim();
 const mysize = document.getElementById("size").value;
@@ -28,7 +30,7 @@ if (mytext !== "") {
     function resetForm() {
       document.getElementById("img").src = "";
       document.getElementById("qrtext").value = "";
-      document.getElementById("size").value = "250";
+      document.getElementById("size").value = "100";
 
       // Show form elements again
       document.getElementById("size").style.display = "inline-block";
